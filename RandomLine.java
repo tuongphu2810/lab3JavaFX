928df483e3602e07339c2b993abfc6a690746668
padjei054-collab <padjei054@gmail.com>
2026-09-16 18:44:40 -0700

    Update RandomLine.java

--- RandomLine.java
+++ RandomLine.java
@@ -1,6 +1,7 @@
 Pane pane = new pane();
 for(int i = 0; i < 10; i++){
   for(int j = 0; j < 10; j++){
-    rectangle rect = new Rectangle ();
-    rect.setX();
-    rect.setY();
+    rectangle rect = new Rectangle (50,50);
+    rect.setX(i * 50);
+    rect.setY(j * 50);
+    rec.setFill(Color.color(MathRandom
